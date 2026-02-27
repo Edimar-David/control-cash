b0355d93204d14d9b5ac0ba75fd2c905c9110bc0
edimar <edimarjr51@gmail.com>
2026-02-26 22:28:28 -0300

    Initial commit

---
+++ src/main/java/com/example/control/cash/ControlCashApplication.java
@@ -0,0 +1,13 @@
+package com.example.control.cash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlCashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlCashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/control/cash/ControlCashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.control.cash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlCashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
